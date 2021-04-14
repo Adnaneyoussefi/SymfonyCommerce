1d7f58108bc3b78eeaa0249a7189c20ddf092896
Adnane Youssefi <adnaneyoussefi70@gmail.com>
2021-04-14 13:55:21 +0000

    Ajouter l'interface RessourceInterface

---
+++ src/Service/CategorieService.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\Categorie;
+use App\Service\RessourceInterface;
+
+class CategorieService implements RessourceInterface {
+
+    /**
+     * @return Categorie[]
+     */
+    public function get($id, $soapClient) {
+       
+    }
+
+    public function delete($id, $soapClient) {
+    }
+
+    public function add() {
+
+    }
+
+    public function update($id, $soapClient) {
+        
+    }
+}
--- src/Service/CommerceAPI.php
+++ src/Service/CommerceAPI.php
@@ -6,14 +6,18 @@ use App\Entity\Produit;
 use App\Entity\Categorie;
 use App\Service\Iservice;
 use App\Service\ProduitService;
+use App\Service\RessourceInterface;
 
 class CommerceAPI implements Iservice {
 
+    private RessourceInterface $ressourceInterface;
+
     private $api_commerce;
 
-    public function __construct(string $api_commerce)
+    public function __construct(string $api_commerce, RessourceInterface $ressourceInterface)
     {
         $this->api_commerce = $api_commerce;
+        $this->ressourceInterface = $ressourceInterface;
     }
 
     public function getModels(): array {
@@ -24,14 +28,14 @@ class CommerceAPI implements Iservice {
 
     public function getModelById($id) {
         $soapClient = new \SoapClient($this->api_commerce);
-        $produitService = new ProduitService();
-        return $produitService->getProduitById($id, $soapClient);
+        //$produitService = new ProduitService();
+        return $this->ressourceInterface->get($id, $soapClient);
     }
 
     public function deleteModelById($id) {
         $soapClient = new \SoapClient($this->api_commerce);
-        $produitService = new ProduitService();
-        return $produitService->deleteProduitById($id, $soapClient);
+        //$produitService = new ProduitService();
+        return $this->ressourceInterface->delete($id, $soapClient);
     }
 
     public function addModel() {
